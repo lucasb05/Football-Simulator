821dcc74507d714815803d478aa0d8677928db4d
lucasb05 <162258930+lucasb05@users.noreply.github.com>
2024-03-04 23:37:16 -0500

    basic outline.py

--- basic outline.py
+++ basic outline.py
@@ -5,16 +5,23 @@ t_list =[]
 player_count = []
 success_chance = []
 for i in range(team_num):
-    t_list.append("Team " +  str(i + 1))
-    player_count.append(random.randint(11, 25))
-    if player_count[i] == 11:
+    t_list.append(str(input("What is the name of your team?: ")))
+    player_count.append(int(input("How many players are on this team?: ")))
+    if player_count[i] < 11:
+        success_chance.append(-1)
+    elif player_count[i] == 11:
         success_chance.append(random.randint(0, 30))
     elif 11 < player_count[i] <= 19:
         success_chance.append(random.randint(30, 70))
-    elif 19 < player_count[i] <= 25:
+    elif 19 < player_count[i]:
         success_chance.append(random.randint(70, 100))
+    elif player_count[i] > 50:
+        success_chance.append(random.randint(98, 100))
 final = []
 for i in range(team_num):
-    final.append(str(t_list[i]) + f' - ' + str(player_count[i]) + f' players with a ' + str(success_chance[i]) + f'% chance of winning')
+    if success_chance[i] == -1:
+        final.append(str(t_list[i]) + f' - ' + str(player_count[i]) + f' players, cannot compete without 11 players')
+    else:
+        final.append(str(t_list[i]) + f' - ' + str(player_count[i]) + f' players with a ' + str(success_chance[i]) + f'% chance of winning')
 for i in range(team_num):
     print(final[i])
